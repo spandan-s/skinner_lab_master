ee869a8e5e2beba7d84dd8a0b4503eac5142befc
spandan-s <95880822+spandan-s@users.noreply.github.com>
2023-03-02 11:53:20 -0500

    Add files via upload

---
+++ in_vitro/abf_metadata.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  5 12:32:14 2022
+
+@author: spandans
+"""
+
+import pyabf
+import os
+import matplotlib.pyplot as plt
+from time import time
+
+def get_abf_metadata(fn, write_to_file=True, Plot=False):
+    save_fn = f'metadata/{fn.split(".")[0]}_metadata.txt'
+
+    start_time = time()
+    abf = pyabf.ABF(fn)
+
+    header = abf.headerText
+    soln = '(in mM): NaCl 125, KCl 3.5, NaH2PO4 1.25, CaCl2 1.5, MgSO4 1.5, NaHCO3 26 \
+    and glucose 10. Aerated with 5%CO2-95%O2 (pH 7.35-7.4).'
+
+    lines = {
+        'RECORDING TYPE': 'monopolar',
+        'SOLUTION': soln,
+        'SAMPLE RATE': str(abf.sampleRate),
+        'FILE COMMENT': abf.abfFileComment,
+        'X LABEL': abf.sweepLabelX,
+        'Y LABEL': abf.sweepLabelY,
+        'X UNITS': abf.sweepUnitsX,
+        'Y UNITS': abf.sweepUnitsY
+    }
+
+    labels = []
+    for key, value in lines.items():
+        labels.append(key)
+
+    if write_to_file:
+        with open(save_fn, 'w') as f:
+            print('=========================================')
+            print('Writing to file:', save_fn)
+
+            f.write('### IMPORTANT STUFF ###\n')
+
+            for label in labels:
+                f.write(label + ': ')
+                f.write(lines[label])
+                f.write('\n')
+
+            f.write('\n### FULL HEADER ###')
+            f.write(header)
+    print(f'Completed in {(time() - start_time):.2f} seconds. Metadata file saved in metadata folder.')
+    print('=========================================\n')
+
+    sweepX = abf.sweepX
+    sweepY = abf.sweepY
+    sweepC = abf.sweepC
+
+    if Plot:
+        plt.figure(figsize=[12, 9])
+        plt.plot(sweepX, sweepY)
+        plt.xlabel(lines['X LABEL'])
+        plt.ylabel(lines['Y LABEL'])
+        plt.title(fn.split('.')[0])
+        plt.show()
+        # plt.xlim(175, 195)
+
+dir = '/home/spandans/Documents/NFRF_testdata_Aylin_Liang//noise_test_march_20_2022/'
+os.chdir(dir)
+
+try:
+    os.mkdir('metadata')
+except:
+    FileExistsError
+
+for fn in sorted(i for i in os.listdir(dir) if i.endswith('.abf')):
+    get_abf_metadata(fn)
+
+# fn = '221004_5901cko_0026.abf'
+# save_fn = fn.split('.')[0] + '_metadata.txt'
+#
+# start_time = time()
+# abf = pyabf.ABF(fn)
+#
+# header = abf.headerText
+# soln = '(in mM): NaCl 125, KCl 3.5, NaH2PO4 1.25, CaCl2 1.5, MgSO4 1.5, NaHCO3 26 \
+# and glucose 10. Aerated with 5%CO2-95%O2 (pH 7.35-7.4).'
+#
+# lines = {
+#     'RECORDING TYPE': 'monopolar',
+#     'SOLUTION': soln,
+#     'SAMPLE RATE': str(abf.sampleRate),
+#     'FILE COMMENT': abf.abfFileComment,
+#     'X LABEL': abf.sweepLabelX,
+#     'Y LABEL': abf.sweepLabelY,
+#     'X UNITS': abf.sweepUnitsX,
+#     'Y UNITS': abf.sweepUnitsY
+#     }
+#
+# labels = []
+# for key, value in lines.items():
+#     labels.append(key)
+#
+# write_to_file = True
+# if write_to_file:
+#     with open(save_fn, 'w') as f:
+#         print('=========================================')
+#         print('Writing to file:', save_fn)
+#
+#         f.write('### IMPORTANT STUFF ###\n')
+#
+#         for label in labels:
+#             f.write(label + ': ')
+#             f.write(lines[label])
+#             f.write('\n')
+#
+#         f.write('\n### FULL HEADER ###')
+#         f.write(header)
+# print(f'Completed in {(time() - start_time):.2f} seconds')
+# print('=========================================\n')
+#
+# sweepX = abf.sweepX
+# sweepY = abf.sweepY
+# sweepC = abf.sweepC
+#
+# Plot = True
+# if Plot:
+#     plt.figure(figsize=[12, 9])
+#     plt.plot(sweepX, sweepY)
+#     plt.xlabel(lines['X LABEL'])
+#     plt.ylabel(lines['Y LABEL'])
+#     plt.title(fn.split('.')[0])
+#     plt.show()
+#     # plt.xlim(175, 195)
---
+++ in_vitro/preprocess_invitro_v2.py
@@ -0,0 +1,201 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyabf
+import os
+from f_filter_lfp import filter_lfp, notch_filt
+from scipy.signal import decimate, find_peaks, periodogram
+
+
+def pre_filter(Y, fs, cutoff=[0.5, 100], w0=60):
+    y_filt_60Hz = notch_filt(Y, fs, 60, Q=50)
+    y_filt = filter_lfp(y_filt_60Hz, fs, cutoff)
+
+    return y_filt
+
+
+def downsample_signal(X, Y, M=50):
+    # down-sample signal to make difference calculation easier
+    ds_Y = decimate(Y, M)
+    newX = np.linspace(X[0], X[-1], len(ds_Y), endpoint=False)
+
+    return newX, ds_Y
+
+
+def find_jumps(Y, fs, threshold=0.025, mask_len=5):
+    # find element-wise difference of signal
+    diff_Y = np.diff(Y)
+
+    # find where abs(difference signal) is greater than the threshold
+    isValid = abs(diff_Y) > threshold
+
+    isValid = np.convolve(isValid, np.ones(mask_len, ), mode='same') > 0
+    isValid = np.clip(isValid, -1, 1)
+
+    d = abs(np.diff(isValid))
+    jumps = np.where(d > 0.5)
+
+    return jumps
+
+
+def cut_signal(X, Y, fs):
+    newX, ds_Y = downsample_signal(X, Y)
+
+    jumps = find_jumps(ds_Y, fs)
+
+    time_jumps = newX[jumps]
+
+    eps = 0.0001
+    cut_pts = np.zeros(len(time_jumps), dtype=int)
+
+    for idx, val in enumerate(time_jumps):
+        x = np.where(abs(X - val) <= eps)[0][0]
+        cut_pts[idx] = x
+
+    cut_pts = np.append(cut_pts, len(X) - 1)
+    # print(X[cut_pts])
+    num_sections = len(cut_pts)
+    min_length = fs * 5
+    max_length = fs * 30
+    cut_signals_X, cut_signals_Y = [], []
+
+    start, stop = 0, cut_pts[0]
+    count = 0
+    for idx in range(num_sections):
+        while (stop - start >= max_length):
+            cutX = np.array(X[start: start + max_length])
+            cutY = np.array(Y[start: start + max_length])
+            cut_signals_X.append(cutX)
+            cut_signals_Y.append(cutY)
+            start = start + max_length
+        if (stop - start >= min_length):
+            cutX = np.array(X[start:stop])
+            cutY = np.array(Y[start:stop])
+            cut_signals_X.append(cutX)
+            cut_signals_Y.append(cutY)
+        count += 1
+        start = stop
+        if count < len(cut_pts):
+            stop = cut_pts[count]
+
+    return cut_signals_X, cut_signals_Y
+
+
+def count_spikes(X, Y):
+    plt.figure()
+    plt.plot(X, Y)
+
+    # find the mean voltage
+    mean_Y = np.mean(Y)
+    plt.axhline(mean_Y + 0.01, color='C1')
+    # find peaks in the delta range that are a certain height above the mean
+    peaks, _ = find_peaks(Y, height=mean_Y + 0.01, distance=6667)
+    plt.plot(X[peaks], Y[peaks], 'x')
+
+    # check if there are at least x events depending on the length of the section
+    num_peaks = len(peaks)
+    text_loc = [120 + 0.75 * np.ptp(X), np.min(Y)]
+    # required number of peaks = 1 peak per 2 seconds => 1 peak per 2*fs samples
+    # for signal of x seconds = x*fs samples => 0.5*x/fs samples
+    req_peaks = round(0.5 * np.ptp(X))
+    plt.title(f'no. of peaks: {num_peaks}/{req_peaks}')
+
+    print(f'no. of peaks: {num_peaks}/{req_peaks}')
+
+    if num_peaks >= req_peaks:
+        return True
+    else:
+        return False
+
+def plot_periodograms(X, Y, fs):
+    delta_range = [0.5, 5]
+    theta_range = [2, 15]
+    gamma_range = [15, 100]
+
+    Y_filt_delta = filter_lfp(Y, fs, delta_range)
+    Y_filt_theta = filter_lfp(Y, fs, theta_range)
+    Y_filt_gamma = filter_lfp(Y, fs, gamma_range)
+
+    fxx_delta, pxx_delta = periodogram(Y_filt_delta, fs)
+    fxx_theta, pxx_theta = periodogram(Y_filt_theta, fs)
+    fxx_gamma, pxx_gamma = periodogram(Y_filt_gamma, fs)
+
+    peak_delta = [fxx_delta[np.argmax(pxx_delta)], np.max(pxx_delta)]
+    peak_theta = [fxx_theta[np.argmax(pxx_theta)], np.max(pxx_theta)]
+    peak_gamma = [fxx_gamma[np.argmax(pxx_gamma)], np.max(pxx_gamma)]
+
+    fig, ax = plt.subplots(1, 3, figsize=[16, 9])
+    ax[0].set_title('Delta Periodogram')
+    ax[0].plot(fxx_delta, pxx_delta)
+    ax[0].set_xlim(-0.5, 20)
+    ax[0].set_xlabel('Frequency [Hz]')
+    ax[0].set_ylabel('Theta Spectral Density')
+
+    ax[1].set_title('Theta Periodogram')
+    ax[1].plot(fxx_theta, pxx_theta)
+    ax[1].set_xlim(-0.5, 20)
+    ax[1].set_xlabel('Frequency [Hz]')
+    ax[1].set_ylabel('Theta Spectral Density')
+
+    ax[2].set_title('Gamma Periodogram')
+    ax[2].plot(fxx_gamma, pxx_gamma)
+    ax[2].set_xlim(10, 120)
+    ax[2].set_xlabel('Frequency [Hz]')
+    ax[2].set_ylabel('Gamma Spectral Density')
+
+    plt.tight_layout()
+    return peak_delta, peak_theta, peak_gamma
+
+def check_validity(X, Y, fs):
+    valid = count_spikes(X, Y)
+
+    if valid:
+        peak_delta, peak_theta, peak_gamma = plot_periodograms(X, Y, fs)
+
+        print(f'Peak Theta: {peak_theta[0]:.2E} Hz; {peak_theta[1]:.2E} mV^2/Hz')
+        print(f'Peak Delta: {peak_delta[0]:.2E} Hz; {peak_delta[1]:.2E} mV^2/Hz')
+        print(f'Peak Gamma: {peak_gamma[0]:.2E} Hz; {peak_gamma[1]:.2E} mV^2/Hz')
+    return valid
+
+# ==========================================================
+# MAIN PROGRAM
+
+# load abf file and X and Y data
+dir = '/home/spandans/Documents/NFRF_testdata_Aylin_Liang/Oct_4_2022_Gg_37/'
+os.chdir(dir)
+
+fn = '221004_5901cko_0001'
+
+# load signal from abf file
+abf = pyabf.ABF(f'{dir}{fn}.abf')
+
+print('==================================================')
+print(f'Processing signal from file: {fn}')
+
+sweepX = abf.sweepX
+sweepY = abf.sweepY
+
+plt.figure()
+plt.plot(sweepX, sweepY)
+
+fs = abf.sampleRate
+
+Y_filt = pre_filter(sweepY, fs)
+
+# cut signal into "valid" chunks
+cut_signal_X, cut_signal_Y = cut_signal(sweepX, Y_filt, fs)
+
+# check validity of each section and plot frequency domain stuff
+num_signals = len(cut_signal_X)
+# print(num_signals)
+for idx in range(num_signals):
+    signal_X, signal_Y = cut_signal_X[idx], cut_signal_Y[idx]
+
+    print('==================================================')
+    print(f'Processing section {idx +1} from {round(signal_X[0], 2)} to {round(signal_X[-1], 2)} seconds')
+
+    validity = check_validity(signal_X, signal_Y, fs)
+    print(f'Signal validity: {validity}')
+    print('==================================================\n')
+
+plt.show()
+# plt.close()
