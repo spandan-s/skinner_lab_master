c96e7376d1dc8a5a116696897ef626771f79a41c
spandans <spandan.m.sengupta@gmail.com>
2023-03-09 16:57:31 -0500

    genetic algorithm modified threshold of htest modified htest code to use cell specific r_o

--- prm/prm_gen_search.py
+++ prm/prm_gen_search.py
@@ -1,10 +1,14 @@
+import json
+
 import numpy as np
 import matplotlib.pyplot as plt
-
+from copy import deepcopy
 from prm_v2 import *
 from prm_htest import hypothesis_test
 
-def gen_search(ref_conns, max_iter=1000):
+rng = np.random.default_rng()
+
+def gen_search(ref_conns, max_iter=100):
     valid_conns = []
     # start at a given point in the parameter space (let: reference parameter)
     #   iniitialise PRM with conns
@@ -15,18 +19,85 @@ def gen_search(ref_conns, max_iter=1000):
     valid_conns.append(ref_conns)
 
     for i in range(max_iter):
+        if i%(max_iter//10) == 0:
+            print(f"iteration {i}")
         # loop through set of valid conns
         for conn in valid_conns:
             #   for each VC, change one of the conns by a random value (within constraints)
-            new_conn = change_one_conn(conn)
+            new_conn = change_conns(conn)
             #   initialise PRM with new conns
             #   run simulation and do spectral analysis
             #   perform hypothesis test
             conn_validity = hypothesis_test(new_conn)
             #       if true: add to set of valid conns
             if conn_validity:
-                valid_conns.append(new_conn)
+                if new_conn not in valid_conns:
+                    valid_conns.append(new_conn)
+                    with open("search_results.json", "w") as w:
+                        json.dump(valid_conns, w)
+                    print(f"{len(valid_conns)} valid configurations found")
 
     # run for either a set number of configurations OR till a certain number of valid configurations are found
+    return valid_conns
 
-def change_one_conn(conns):
+def change_conns(conns, I):
+    nrng = np.random.default_rng()
+
+    c_list = ["pyr", "bic", "pv", "cck"]
+    new_conns = deepcopy(conns)
+    new_I = deepcopy(I)
+
+    while new_conns == conns:
+        n_mutations = rng.poisson(2)
+        for i in range(n_mutations):
+            if bool(rng.integers(0, 2)):
+                c1, c2 = c_list[nrng.integers(4)], c_list[nrng.integers(4)]
+                new_conns[c1][c2] *= np.linspace(0.5, 1.5, 10)[nrng.integers(10)]
+        else:
+            c1 = c_list[nrng.integers(4)]
+            i_constraints = {
+                "pyr": [0, 0.1],
+                "bic": [-2, -1],
+                "pv": [0.5, 1],
+                "cck": [0.25, 1]
+            }
+            new_I[c1] = nrng.uniform(*i_constraints[c1])
+
+        # print(new_conns == conns)
+
+    return new_conns, new_I
+
+# Parameters
+T = 8.0  # total time (units in sec)
+dt = 0.001  # plotting and Euler timestep (parameters adjusted accordingly)
+fs = 1 / dt
+
+# FI curve
+beta = 10
+tau = 5
+h = 0
+# r_o = 30
+
+c_list = ["pyr", "bic", "pv", "cck"]
+
+ref_prm = PRM_v2()
+
+#
+# for i in range(10):
+#     c1, c2 = c_list[rng.integers(4)], c_list[rng.integers(4)]
+#     # print(c1, c2)
+#     new_conns["pyr"]["bic"] *= [0.5, 2][rng.integers(2)]
+#     print(new_conns == conns)
+
+
+# new_conn = change_one_conn(ref_prm.conns)
+# print(new_conn)
+
+search_results = gen_search(ref_prm.conns)
+
+# with open("search_results.json", "w") as final:
+#     json.dump(search_results, final)
+
+
+# print(change_conns(ref_prm.conns))
+
--- prm/prm_htest.py
+++ prm/prm_htest.py
@@ -7,89 +7,101 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 def hypothesis_test(conns="default", I="default", plot=False):
+    # ===============================================================
+    new_prm = PRM_v2()
+
+    time = np.arange(0, T, dt)
+
+    new_prm.set_init_state(len(time))
+    new_prm = simulate(time, new_prm, dt, tau)
+    # =================================================================
+    dps_tpp = calc_spectral(new_prm.R, fs, time, new_prm.labels, 'theta', 'power')["pyr"]
+    dps_gpp = calc_spectral(new_prm.R, fs, time, new_prm.labels, 'gamma', 'power')["pyr"]
+    # =================================================================
+
     # Checking validity of parameter set
     test_prm = PRM_v2(conns, I)
     h_test = [False] * 5
     # new_conns = test_prm.conns.copy()
 
     test_prm.set_init_state(len(time))
-    test_prm = simulate(time, test_prm, dt, tau, r_o)
+    test_prm = simulate(time, test_prm, dt, tau)
     if plot:
         plot_trace(time, test_prm.R, test_prm.labels)
 
     # Check if it satisfies primary hypothesis
     #   Has theta power >= 50% of default parameter set (DPS)
     #   Has gamma power >= 25% of DPS
-    print("Checking primary hypothesis")
+    # print("Checking primary hypothesis")
     pH0 = np.zeros(2)
     pH0[0] = calc_spectral(test_prm.R, fs, time, test_prm.labels, 'theta', 'power')["pyr"]
     pH0[1] = calc_spectral(test_prm.R, fs, time, test_prm.labels, 'gamma', 'power')["pyr"]
-    if (pH0[0] >= (0.5 * dps_tpp)) and (pH0[1] >= (0.5 * dps_gpp)):
+    if (pH0[0] >= (0.4 * dps_tpp)) and (pH0[1] >= (0.4 * dps_gpp)):
         h_test[0] = True
 
         # Check if it satisfies secondary hypothesis #1 (removal of PYR->PYR connections)
         #   Has theta power <= 10% of DPS
-        print("Checking secondary hypothesis #1")
+        # print("Checking secondary hypothesis #1")
         test_prm.conns["pyr"]["pyr"] = 0
         test_prm.set_init_state(len(time))
-        test_prm = simulate(time, test_prm, dt, tau, r_o)
+        test_prm = simulate(time, test_prm, dt, tau)
         pH1 = calc_spectral(test_prm.R, fs, time, test_prm.labels, 'theta', 'power')["pyr"]
-        if pH1 <= 0.1 * dps_tpp:
+        if pH1 <= 0.15 * dps_tpp:
             h_test[1] = True
 
             # Check if it satisfies secondary hypothesis #2 (removal of CCK->PV connections)
             #   Has theta power <= 10% of DPS
-            print("Checking secondary hypothesis #2")
+            # print("Checking secondary hypothesis #2")
             test_prm.set_connections(conns)
             test_prm.conns["cck"]["pv"] = 0
             test_prm.set_init_state(len(time))
-            test_prm = simulate(time, test_prm, dt, tau, r_o)
+            test_prm = simulate(time, test_prm, dt, tau)
             pH2 = calc_spectral(test_prm.R, fs, time, test_prm.labels, 'theta', 'power')["pyr"]
-            if pH2 <= 0.1 * dps_tpp:
+            if pH2 <= 0.15 * dps_tpp:
                 h_test[2] = True
 
                 # Check if it satisfies secondary hypothesis #3 (removal of PV->PYR connections)
                 #   Has theta power <= 10% of DPS
-                print("Checking secondary hypothesis #3")
+                # print("Checking secondary hypothesis #3")
                 test_prm.set_connections(conns)
                 test_prm.conns["pv"]["pyr"] = 0
                 test_prm.set_init_state(len(time))
-                test_prm = simulate(time, test_prm, dt, tau, r_o)
+                test_prm = simulate(time, test_prm, dt, tau)
                 pH3 = calc_spectral(test_prm.R, fs, time, test_prm.labels, 'theta', 'power')["pyr"]
-                if pH3 <= 0.1 * dps_tpp:
+                if pH3 <= 0.15 * dps_tpp:
                     h_test[3] = True
 
                     # Check if it satisfies secondary hypothesis #4 (removal of BiC->PYR connections)
                     #   Has theta power <= 10% of DPS
-                    print("Checking secondary hypothesis #4")
+                    # print("Checking secondary hypothesis #4")
                     test_prm.set_connections(conns)
                     test_prm.conns["bic"]["pyr"] = 0
                     test_prm.set_init_state(len(time))
-                    test_prm = simulate(time, test_prm, dt, tau, r_o)
+                    test_prm = simulate(time, test_prm, dt, tau)
                     pH4 = calc_spectral(test_prm.R, fs, time, test_prm.labels, 'theta', 'power')["pyr"]
-                    if pH4 <= 0.1 * dps_tpp:
+                    if pH4 <= 0.15 * dps_tpp:
                         h_test[4] = True
                     else:
-                        print("Failed secondary hypothesis #4 (removal of BiC->PYR connections)")
+                        # print("Failed secondary hypothesis #4 (removal of BiC->PYR connections)")
                         return False
                 else:
-                    print("Failed secondary hypothesis #3 (removal of PV->PYR connections)")
+                    # print("Failed secondary hypothesis #3 (removal of PV->PYR connections)")
                     return False
             else:
-                print("Failed secondary hypothesis #2 (removal of CCK->PV connections)")
+                # print("Failed secondary hypothesis #2 (removal of CCK->PV connections)")
                 return False
         else:
-            print("Failed secondary hypothesis #1 (removal of PYR->PYR connections)")
+            # print("Failed secondary hypothesis #1 (removal of PYR->PYR connections)")
             return False
     else:
-        print("Failed primary hypothesis")
+        # print("Failed primary hypothesis")
         return False
 
-    print("Valid reference parameter set")
+    # print("Valid reference parameter set")
     return True
 # =============================================================
 # Parameters
-T = 2.0  # total time (units in sec)
+T = 8.0  # total time (units in sec)
 dt = 0.001  # plotting and Euler timestep (parameters adjusted accordingly)
 fs = 1 / dt
 
@@ -97,25 +109,25 @@ fs = 1 / dt
 beta = 10
 tau = 5
 h = 0
-r_o = 30
+# r_o = 30
 
 c_list = ["pyr", "bic", "pv", "cck"]
-# ===============================================================
-new_prm = PRM_v2()
+# # ===============================================================
+# new_prm = PRM_v2()
+#
+# time = np.arange(0, T, dt)
+#
+# new_prm.set_init_state(len(time))
+# new_prm = simulate(time, new_prm, dt, tau, r_o)
+# # =================================================================
+# dps_tpp = calc_spectral(new_prm.R, fs, time, new_prm.labels, 'theta', 'power')["pyr"]
+# dps_gpp = calc_spectral(new_prm.R, fs, time, new_prm.labels, 'gamma', 'power')["pyr"]
+# # =================================================================
 
-time = np.arange(0, T, dt)
-
-new_prm.set_init_state(len(time))
-new_prm = simulate(time, new_prm, dt, tau, r_o)
-# =================================================================
-dps_tpp = calc_spectral(new_prm.R, fs, time, new_prm.labels, 'theta', 'power')["pyr"]
-dps_gpp = calc_spectral(new_prm.R, fs, time, new_prm.labels, 'gamma', 'power')["pyr"]
-# =================================================================
-
-new_conns = new_prm.conns.copy()
-new_I = new_prm.I.copy()
-
-chn = [0.5, 1, 2]
+# new_conns = new_prm.conns.copy()
+# new_I = new_prm.I.copy()
+#
+# chn = [0.5, 1, 2]
 # for c1 in c_list:
 #     new_I[c1] /= chn[np.random.randint(3)]
 #     for c2 in c_list:
@@ -145,29 +157,29 @@ chn = [0.5, 1, 2]
 
 
 
-new_conns = {
-    'pyr': {'pyr': 0.03, 'bic': 0.04, 'pv': 0.02, 'cck': 0.0},
-    'bic': {'pyr': -0.06, 'bic': 0.0, 'pv': 0.0, 'cck': 0.0},
-    'pv': {'pyr': -0.04, 'bic': 0.0, 'pv': -0.055, 'cck': -0.15},
-    'cck': {'pyr': 0.0, 'bic': 0.0, 'pv': -0.3, 'cck': -0.075}
-}
-new_I = {
-    'pyr': 0.07, 'bic': -0.525, 'pv': 0.225, 'cck': 1.4
-}
+# new_conns = {
+#     'pyr': {'pyr': 0.03, 'bic': 0.04, 'pv': 0.02, 'cck': 0.0},
+#     'bic': {'pyr': -0.06, 'bic': 0.0, 'pv': 0.0, 'cck': 0.0},
+#     'pv': {'pyr': -0.04, 'bic': 0.0, 'pv': -0.055, 'cck': -0.15},
+#     'cck': {'pyr': 0.0, 'bic': 0.0, 'pv': -0.3, 'cck': -0.075}
+# }
+# new_I = {
+#     'pyr': 0.07, 'bic': -0.525, 'pv': 0.225, 'cck': 1.4
+# }
 
 #
 #
 # hypothesis_test(new_conns, new_I, True)
 #
-p2 = PRM_v2(new_conns, new_I)
-p2.set_init_state(len(time))
-p2 = simulate(time, p2, dt, tau, r_o)
-
-p2_gpp = calc_spectral(p2.R, fs, time, p2.labels, 'gamma', 'power', plot_Fig=True, plot_Filter=True)["pyr"]
-print(dps_gpp, p2_gpp, (p2_gpp/dps_gpp)*100)
-
-plot_trace(time, p2.R, p2.labels)
-# #
+# p2 = PRM_v2(new_conns, new_I)
+# p2.set_init_state(len(time))
+# p2 = simulate(time, p2, dt, tau, r_o)
+#
+# p2_gpp = calc_spectral(p2.R, fs, time, p2.labels, 'gamma', 'power', plot_Fig=True, plot_Filter=True)["pyr"]
+# print(dps_gpp, p2_gpp, (p2_gpp/dps_gpp)*100)
+#
+# plot_trace(time, p2.R, p2.labels)
+# # #
 # p2.set_connections(new_conns)
 # # p2.conns["cck"]["pv"] = 0
 # p2.set_init_state(len(time))
@@ -179,4 +191,4 @@ plot_trace(time, p2.R, p2.labels)
 # print(f"dps ttp: {dps_tpp}")
 # print(f"no_cck_pv_tpp: {new_tpp}")
 
-plt.show()
+# plt.show()
